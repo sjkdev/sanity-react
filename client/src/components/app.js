1f80cc9a9024b245f5344e3ebc8c931c1eea17ea
seanjamesking <helloseanking@yahoo.com>
2020-12-05 16:26:29 +0000

    cleaned up app.js file

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,10 +2,7 @@ import React, { Fragment } from "react";
 import Header from './layout/Header'
 import Navigation from './layout/Navigation'
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Styles from './assets/sass/global.scss'
 import "./assets/sass/global.scss";
-
-// import imgFile from "../../assests/image/Favicon.png";
 
 const App = () => {
   return (
